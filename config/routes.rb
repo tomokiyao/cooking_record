cf61ec849f450dd30e9cadd58f70864abf049b42
tomoki.yao <tomoki9226@gmail.com>
2021-05-04 00:03:10 +0900

    レシピ画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :my_cookings, only: :index
+  resources :recipes, only: :index
+  
   root to: 'my_cookings#index'
 
   namespace :api do
