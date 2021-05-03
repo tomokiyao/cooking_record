158b5d7291e61a28594edb85460ea71e639345e0
tomoki.yao <tomoki9226@gmail.com>
2021-05-03 22:03:05 +0900

    料理記録画面作成

--- app/controllers/api/internal/fetch_cooking_records_controller.rb
+++ app/controllers/api/internal/fetch_cooking_records_controller.rb
@@ -2,7 +2,9 @@ module Api
   module Internal
     class FetchCookingRecordsController < ApplicationController
       def index
-        uri = URI("https://cooking-records.herokuapp.com/cooking_records")
+        uri = URI(
+          "https://cooking-records.herokuapp.com/cooking_records?offset=#{params[:offset]}&limit=6"
+        )
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         req = Net::HTTP::Get.new(uri)
--- app/javascript/packs/cooking.js
+++ app/javascript/packs/cooking.js
@@ -1,27 +1,66 @@
 import Vue from 'vue/dist/vue.esm';
 import { RECIPE_TYPES } from './constant';
 import axios from 'axios';
+import InfiniteLoading from 'vue-infinite-loading';
+Vue.use(InfiniteLoading);
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#cooking',
     data: {
       cooking_records: [],
+      main_dish_records: [],
+      side_dish_records: [],
+      soup_records: [],
+      records_total_count: 0,
+      offset: 0,
+      initialized: false,
     },
     created() {
-      axios
-        .get('/api/internal/fetch_cooking_records')
-        .then(
-          (response) =>
-            (this.cooking_records = [
-              ...this.cooking_records,
-              ...response.data.cooking_records,
-            ])
-        );
+      this.fetchCookingRecords();
     },
     methods: {
       castRecipeType(type) {
         return RECIPE_TYPES[type];
+      },
+      fetchCookingRecords() {
+        const params = {
+          offset: this.offset,
+        };
+        axios
+          .get('/api/internal/fetch_cooking_records', { params })
+          .then((response) => {
+            this.cooking_records = [
+              ...this.cooking_records,
+              ...response.data.cooking_records,
+            ];
+            this.partRecipeType(response.data.cooking_records);
+            this.records_total_count = response.data.pagination.total;
+            this.offset = this.offset + 6;
+            this.initialized = true;
+          });
+      },
+      partRecipeType(records) {
+        const mainDish = records.filter(
+          (record) => record.recipe_type === 'main_dish'
+        );
+        const sideDish = records.filter(
+          (record) => record.recipe_type === 'side_dish'
+        );
+        const soup = records.filter((record) => record.recipe_type === 'soup');
+        this.main_dish_records = [...this.main_dish_records, ...mainDish];
+        this.side_dish_records = [...this.side_dish_records, ...sideDish];
+        this.soup_records = [...this.soup_records, ...soup];
+      },
+      infiniteHandler($state) {
+        if (this.cooking_records.length === this.records_total_count) {
+          $state.complete();
+        } else {
+          setTimeout(() => {
+            this.fetchCookingRecords();
+            $state.loaded();
+          }, 1500);
+        }
       },
     },
   });
